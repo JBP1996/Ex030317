9139a4ca94045a1cc1bc40effcf2bb9ac821c7ad
jbp19 <jbp19@JBP1996-PC>
2017-03-05 23:21:23 +0000

    Exercicio 2 - Incompleto

--- src/ex030317/Aluno.java
+++ src/ex030317/Aluno.java
@@ -6,44 +6,85 @@
 package ex030317;
 
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.TreeSet;
 
 /**
  *
  * @author JBP1996 Atec
  */
 public class Aluno {
+    
     private String nome;
     private int nota;
     private String contacto;
-    ArrayList[][] aluno;
-
-    public Aluno() {
-           aluno[0][0].add("Joaquim");
-           aluno[0][1].add(15);
-           aluno[0][2].add("915245415");
-           
-           aluno[1][0].add("Filipa");
-           aluno[1][1].add(10);
-           aluno[1][2].add("9124587963");
-           
-           aluno[2][0].add("Ruben");
-           aluno[2][1].add(17);
-           aluno[2][2].add("926524112");
-           
-           aluno[3][0].add("Raquel");
-           aluno[3][1].add(14);
-           aluno[3][2].add("9542147458");
-           
-           aluno[4][0].add("Mariana");
-           aluno[4][1].add(9);
-           aluno[4][2].add("123689643");
+    
+    public Aluno(){
+        setAluno("Ruben",16,"9154254652");
+        setAluno("Fred",17,"915441652");
+        setAluno("Maria",12,"965214252");
+        setAluno("Filipa",14,"911234652");
+        setAluno("Rafael",18,"925488244");
     }
     
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setAluno(String nome,int nota,String contacto){
+        this.nome=nome;
+        this.nota=nota;
+        this.contacto=contacto;
+        nAluno.add(getNome()+" "+getNota()+" "+getContacto());
+    }
+    
+    ArrayList<Object> nAluno= new ArrayList<>();    
+    
     public void ordenar(){
-        ArrayList temp = new ArrayList();
-        for(int i=0;i<aluno[i].length;i++){
-            System.out.println(i);
+        Stack a = new Stack();
+   
+        for(int i=0;i<nAluno.size();i++){
+            System.out.println(nAluno.get(i));
         }
+        
+        for(int i=0;i<nAluno.size();i++){
+            int numero=Integer.parseInt(String.valueOf(nAluno.get(i)).substring(String.valueOf(nAluno.get(i)).indexOf(" ")+1,String.valueOf(nAluno.get(i)).lastIndexOf(" ")));
+            if(i==0){
+               a.push(numero); 
+            }else{
+                //System.out.println("aa"+Integer.parseInt(String.valueOf(a.get(i-1))));
+                int nTroca=Integer.parseInt(String.valueOf(a.get(i-1)));
+                if(nTroca<numero){
+                    for(int x=i;x<a.size();x++){
+
+                    }
+                    a.add(0,numero);
+                }else{
+                    a.add(numero);
+                }
+            }
+            
+            //a.push(Integer.parseInt(String.valueOf(nAluno.get(i)).substring(String.valueOf(nAluno.get(i)).indexOf(" ")+1,String.valueOf(nAluno.get(i)).lastIndexOf(" "))));
+            
+        }
+        
+        for(int i=0;i<a.size();i++){
+            System.out.println(a.get(i));
+        }
+        
+        /*
+        for(int i=0;i<nAluno.size();i++){
+            System.out.println(nAluno.get(i));
+        }
+        */
     }
     
 }
